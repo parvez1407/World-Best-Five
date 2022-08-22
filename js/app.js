484625a23ed5a75d25931c4256dc7c7e9703b3d0
parvez1407 <info.nahidwd@gmail.com>
2022-08-22 17:39:45 +0600

    Final calculation done and validation done

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ function displayPlayer(nameList) {
     }
 }
 
-
 function addToPlayerList(element) {
     // console.log(element.parentNode.parentNode.children[1].innerText);
     const playerName = element.parentNode.parentNode.children[1].innerText;
@@ -37,25 +36,29 @@ function addToPlayerList(element) {
     element.style.backgroundColor = '#808080';
 }
 
+// function declare to get all input value
 function getInputValueById(inputId) {
     const inputField = document.getElementById(inputId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseInt(inputFieldValueString);
+    inputField.value = '';
     return inputFieldValue;
 }
 
+// function declare to get Element value
 function getElementValueById(elementId) {
-    const elementField = getElementById(elementId);
+    const elementField = document.getElementById(elementId);
     const elementFieldValueString = elementField.innerText;
     const elementValue = parseInt(elementFieldValueString);
     return elementValue;
 }
-
+// function declare to set Element value
 function setElementValueById(elementId, finalValue) {
     const calculationValue = document.getElementById(elementId);
     calculationValue.innerText = finalValue;
 }
 
+// function declare for calculate all selected players cost
 function calculatePlayerCostTotal() {
     const perPlayerCostInputValue = getInputValueById('cost-per-player');
     if (isNaN(perPlayerCostInputValue)) {
@@ -74,14 +77,30 @@ function calculatePlayerCostTotal() {
     }
 
 }
-
+// function declare for final calculation for team cost
 function calculateFinalCostTotal() {
-    const
+    // player Expenses
+    const playerTotalExpense = getElementValueById('players-expenses');
+    // player Expenses
+    const managerExpense = getInputValueById('manager-expenses');
+    // player Expenses
+    const coachExpense = getInputValueById('coach-expenses');
+    if (isNaN(managerExpense, coachExpense)) {
+        alert('Please provide valid input')
+        return;
+    }
+
+    const finalTotal = playerTotalExpense + managerExpense + coachExpense;
+
+    setElementValueById('final-total', finalTotal)
+
 }
 
+// Event Lister for calculate button
 document.getElementById('btn-calculate').addEventListener('click', function () {
     calculatePlayerCostTotal();
 })
+// Event Lister for Total Calculate button
 document.getElementById('calculate-final-total').addEventListener('click', function () {
     calculateFinalCostTotal();
 })
